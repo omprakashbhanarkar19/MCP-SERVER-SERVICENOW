d32d50321b8d3f2b41659a94817d6c75e448191b
Bhanarkar <omprakash.bhanarkar@capgemini.com>
2025-12-31 11:49:24 +0530

    added servicenoe mcp server project

--- AWS-EC2-MCP-SERVER/aws-inetgration-server/config.py
+++
@@ -1,6 +0,0 @@
-CPU_THRESHOLD = 80
-MEMORY_THRESHOLD = 75
-METRIC_PERIOD = 300  # seconds
-
-AWS_MCP_URL = "http://localhost:3333"     # AWS EC2 MCP Server URL
-SNOW_MCP_URL = "https://dev181732.service-now.com"  # SERVIVE NOW URL
--- AWS-EC2-MCP-SERVER/aws-inetgration-server/decision_engine.py
+++
@@ -1,4 +0,0 @@
-def should_restart(cpu, memory):
-    if cpu > 80 and memory > 75:
-        return True
-    return False
